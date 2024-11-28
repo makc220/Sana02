d1f841a1d7dfa0d27bed70f6c1beeb05c5345dae
Максим Бондарчук <bondarcumk086@gmail.com>
2024-11-28 12:44:59 +0200

    Завдання 3: Додав проект SwitchValues

---
+++ SwitchValues/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введіть ціле число (0-9): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+    case 0: Console.WriteLine("нуль"); break;
+    case 1: Console.WriteLine("один"); break;
+    case 2: Console.WriteLine("два"); break;
+    case 3: Console.WriteLine("три"); break;
+    case 4: Console.WriteLine("чотири"); break;
+    case 5: Console.WriteLine("п'ять"); break;
+    case 6: Console.WriteLine("шість"); break;
+    case 7: Console.WriteLine("сім"); break;
+    case 8: Console.WriteLine("вісім"); break;
+    case 9: Console.WriteLine("дев'ять"); break;
+    default: Console.WriteLine("Число не входить у діапазон 0-9."); break;
+}
