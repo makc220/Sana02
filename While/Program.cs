301c9be9b83dcc431e05468fd26aacd10a9c6458
Максим Бондарчук <bondarcumk086@gmail.com>
2024-11-28 12:58:50 +0200

    Завдання 4: Додав проект While

---
+++ While/Program.cs
@@ -0,0 +1,49 @@
+﻿
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введіть натуральне число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"a = {CalculateA(n)}");
+Console.WriteLine($"b = {CalculateB(n)}");
+Console.WriteLine($"c = {CalculateC(n)}");
+
+static double CalculateA(int n)
+{
+    double result = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        result += Math.Pow(-1, i) / (2 * i + 1);
+    }
+    return result;
+}
+
+static double CalculateB(int n)
+{
+    double result = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        result += 1 + 1.0 / (i * i);
+    }
+    return result;
+}
+
+static long CalculateC(int n)
+{
+    long result = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        result += Factorial(i);
+    }
+    return result;
+}
+
+static long Factorial(int number)
+{
+    long fact = 1;
+    for (int i = 2; i <= number; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
